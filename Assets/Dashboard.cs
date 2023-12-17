eb3d327443ecd69523a41328d694582d07ba0678
2303216 <145557049+2303216@users.noreply.github.com>
2023-12-17 10:30:43 +0000

    button and form for Dashboard(AddAsset) was edited to account for changes

--- Assets/Dashboard.cs
+++ Assets/Dashboard.cs
@@ -205,25 +205,28 @@ namespace Assets
         {
 
 
-          
-            
-                // Create an instance of HardwareInfo
-                HardwareInfo hardwareInfo = new HardwareInfo();
 
-                // Retrieve hardware information
-                string systemName = hardwareInfo.GetSystemName();
-                string systemModel = hardwareInfo.GetSystemModel();
-                string systemManufacturer = hardwareInfo.GetSystemManufacturer();
-                string systemType = hardwareInfo.GetSystemType();
-                string ipAddress = hardwareInfo.GetLocalIPAddress();
 
-                // Set the text of the textboxes
-                textBox1.Text = systemName;
-                textBox2.Text = systemModel;
-                textBox3.Text = systemManufacturer;
-                textBox4.Text = systemType;
-                textBox8.Text = ipAddress;
-            
+
+
+
+            // Create an instance of HardwareInfo
+            HardwareInfo hardwareInfo = new HardwareInfo();
+
+            // Retrieve hardware information
+            string systemName = hardwareInfo.GetSystemName();
+            string systemModel = hardwareInfo.GetSystemModel();
+            string systemManufacturer = hardwareInfo.GetSystemManufacturer();
+            string systemType = hardwareInfo.GetSystemType();
+            string ipAddress = hardwareInfo.GetLocalIPAddress();
+
+            // Set the text of the textboxes
+            textBox1.Text = systemName;
+            textBox2.Text = systemModel;
+            textBox3.Text = systemManufacturer;
+            textBox4.Text = systemType;
+            textBox8.Text = ipAddress;
+
 
 
         }
